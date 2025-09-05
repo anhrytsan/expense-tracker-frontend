358f89e512c4cfc2e3a9f75500bf05149bdc22b5
Andriian <an.hrytsan@gmail.com>
2025-09-05 16:23:01 +0300

    added notification errors to login page

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router'; // Додали RouterLink
 import { AuthService } from '../../../core/services/auth.service';
+import { NotificationService } from '../../../core/services/notification.service'; // <-- 1. Імпортуємо сервіс
 
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,12 +10,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    RouterLink,
+    RouterLink, // Додали RouterLink
     ReactiveFormsModule,
     MatCardModule,
     MatFormFieldModule,
@@ -29,6 +29,7 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
   private router = inject(Router);
+  private notificationService = inject(NotificationService); // <-- 2. Інжектуємо сервіс
 
   // Fields will never be null
   loginForm = this.fb.nonNullable.group({
@@ -44,8 +45,10 @@ export class LoginComponent {
           // Navigate to dashboard on successful login
           this.router.navigate(['/dashboard']);
         },
+        // --- 3. Змінюємо цей блок ---
         error: (err) => {
-          console.error('Login failed:', err);
+          const message = err.error.message || 'Невідома помилка входу';
+          this.notificationService.showError(message);
         },
       });
     }
